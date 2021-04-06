7a607437da4e3935a7ce241497693c52e170abb6
Teodora Mihalachi <Teodora.Mihalachi@nxp.com>
2021-04-06 18:09:30 +0300

    [AMPT-6558] Bamboo classes into atlassian.py

---
+++ helper/debugging.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python -tt
+# -*- coding: utf-8 -*-
+
+
+__copyright__ = "2019 NXP Semiconductors. All rights reserved."
+
+
+from pprint import pprint
+
+
+class FunctionDebug(object):
+    """Debug a function.
+
+    This decorator is used for debugging a function, with normal arguments, i.e, not printing out the data of the
+    class it's contained in.
+
+    Keyword arguments:
+    debug -- Whether or not you want to debug the function.
+    """
+    def __init__(self, debug):
+        self.__debug = debug
+
+    def __call__(self, function):
+        def wrapper(*args, **kwargs):
+            if self.__debug:
+                pprint(args)
+                pprint(kwargs)
+
+            return function(*args, **kwargs)
+
+        return wrapper
+
+
+class MethodDebug(object):
+    """Debug a class method.
+
+    This decorator is used for debugging a class method, with normal arguments, and self. When using this decorator,
+    the method will print out it's arguments and the attributes of the class it's contained in.
+
+    Keyword arguments:
+    debug -- Whether or not you want to debug the method.
+    """
+    def __init__(self, debug):
+        self.__debug = debug
+
+    def __call__(self, function):
+        def wrapper(function_self, *args, **kwargs):
+            if self.__debug:
+                pprint(function_self.__dict__)
+                pprint(args)
+                pprint(kwargs)
+
+            return function(function_self, *args, **kwargs)
+
+        return wrapper
